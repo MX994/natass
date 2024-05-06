0bf783ac0023352b4a02f291c136c3ccc9e56d7f
Maximillian Sonderegger <maxmsonderegger@gmail.com>
2024-05-06 18:55:02 -0400

    add sh implant

--- C2_Implant.c
+++ C2_Implant.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <strings.h>
 
 
 int main() {
